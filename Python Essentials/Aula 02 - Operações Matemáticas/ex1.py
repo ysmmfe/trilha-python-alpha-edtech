99850a470dc6beb1f7c51b0f09c770ce6c324837
yasmim <84413595+ysmmfe@users.noreply.github.com>
2023-11-13 11:09:42 -0300

    Delete Python Essentials/Aula 02 - Operações Matemáticas directory

--- Python Essentials/Aula 02 - Operações Matemáticas/ex1.py
+++
@@ -1,6 +0,0 @@
-import math
-
-raio = float(input("Informe o valor do raio: "))
-area = math.pi * (raio**2)
-
-print(f"A área da circunferência é {area:.4f}")
--- Python Essentials/Aula 02 - Operações Matemáticas/ex3.py
+++
@@ -1,6 +0,0 @@
-celsius = int(input("Informe a temperatura em °C: "))
-
-fahrenheit = celsius * 1.8 + 32
-kelvin = celsius + 273.15
-
-print(f"{celsius}°C são {fahrenheit}°F e {kelvin}°K")
