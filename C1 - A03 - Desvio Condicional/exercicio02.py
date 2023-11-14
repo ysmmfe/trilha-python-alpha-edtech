bb02e67d9f5b16e716616ef074544c01c36482fb
yasmim <yasmim.ferreira.200583@gmail.com>
2023-11-14 19:54:11 -0300

    02-slicing-strings

--- C1 - A03 - Desvio Condicional/exercicio02.py
+++ C1 - A03 - Desvio Condicional/exercicio02.py
@@ -6,7 +6,7 @@
 # semelhantes usando técnicas de fatiamento (slicing) de strings. Explique como você pode exibir partes específicas de uma string
 # usando slicing e, em seguida, demonstre isso com um exemplo (dica: pesquisar como fazer slicing de uma string em Python?).
 
-# Explicação:
+# Explicação: 
 # É possível exibir partes específicas de uma string utilizando a técnica slicing, com formato de parâmetros nome_string = [a:b:c],
 # onde "a" é o ponto inicial do "corte" da string, "b" é o ponto final do corte que finaliza o intervalo desejado e "c" é o
 # parâmetrode incremento, que determina o intervalo entre os números do intervalo principal do fatiamento. Segue em anexo o exemplo
