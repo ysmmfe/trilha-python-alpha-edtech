ebfd50fe6906c8bde12ebaaae1faf4ea46889668
yasmim <yasmim.ferreira.200583@gmail.com>
2023-11-14 18:50:57 -0300

    01-concatenacao-string

---
+++ C1 - A03 - Desvio Condicional/exercicio01.py
@@ -0,0 +1,15 @@
+# Trilha Python - Alpha EdTech
+# Autor: https://github.com/ysmmfe
+
+# C1 > Python Essentials > Aula 03 - Questão 1:
+# Leia o primeiro nome do usuário e o último nome do usuário (o usuário sabe que não pode colocar espaço em branco nem sinais especiais),
+# (o usuário é obediente). Montar o email do usuário como nome.sobrenome@gmail.com e pesquise como concatenar strings no Python.
+
+primeiroNome = input("Informe o seu primeiro nome: ")
+ultimoNome = input("Agora informe o seu último nome: ")
+
+# utilizando f string
+#print(f"O seu email será: {primeiroNome}.{ultimoNome}@gmail.com")
+
+# Utilizando concatenação de string, como exemplificado na questão
+print("\nO seu e-mail será: " + primeiroNome + "." + ultimoNome + "@gmail.com")
