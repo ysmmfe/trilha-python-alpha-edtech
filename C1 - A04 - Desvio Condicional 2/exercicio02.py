9678c66c13ed45511ff00b1fc7c500a78743e78e
yasmim <yasmim.ferreira.200583@gmail.com>
2023-11-16 14:15:36 -0300

    02-media-notas

---
+++ C1 - A04 - Desvio Condicional 2/exercicio02.py
@@ -0,0 +1,22 @@
+# Trilha Python - Alpha EdTech
+# Autor: https://github.com/ysmmfe
+
+# C1 > Python Essentials > Aula 04 - Questão 1:
+# Faça um programa para leitura de três notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
+# A mensagem "Aprovado", se a média for maior ou igual a 7, com a respectiva média alcançada;
+# A mensagem "Reprovado", se a média for menor do que 7, com a respectiva média alcançada;
+# A mensagem "Aprovado com Distinção", se a média for igual a 10.
+
+nota1 = float(input("Informe a primeira nota: "))
+nota2 = float(input("Informe a segunda nota: "))
+nota3 = float(input("Informe a terceira nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+if media==10 :
+    print("\nAprovado com distinção")
+else:
+    if media>=7:
+        print(f"\nAprovado \t- Média: {media:.2f}")
+    else:
+        print(f"\nReprovado \t- Média: {media:.2f}")
