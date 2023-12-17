792512db18fcd9dbb19492aed73d7ae7b031579e
yasmim <yasmim.ferreira.200583@gmail.com>
2023-12-16 23:52:04 -0300

    10-eleicao-votos-candidato

---
+++ C1 M1 - Python Essentials/C1 - A05 - Repetição Controlada em Python/exercicio10.py
@@ -0,0 +1,29 @@
+# Trilha Python - Alpha EdTech
+# Autor: https://github.com/ysmmfe
+
+# Ciclo 1 > Módulo 1 - Python Essentials > Aula 05 - Repetição Controlada em Python > Questão 10:
+# Numa eleição existem três candidatos. Faça um programa que peça o número total de eleitores. Peça para cada eleitor votar e
+# ao final mostrar o número de votos de cada candidato.
+
+candidato1 = 0
+candidato2 = 0
+candidato3 = 0
+
+eleitores = int(input("Informe o número total de eleitores: "))
+
+for i in range(1, (eleitores+1)):
+    voto = input(f"\n{i}° eleitor, digite o número do candidato para votar: [1, 2 ou 3] -> ")
+    
+    if voto=='1':
+        candidato1 = candidato1 + 1
+        
+    if voto=='2':
+        candidato2 = candidato2 + 1
+        
+    if voto=='3':
+        candidato3 = candidato3 + 1
+        
+print("\nO número de votos de cada candidato foi:")
+print(f"Candidato 1: {candidato1} voto(s)")
+print(f"Candidato 2: {candidato2} voto(s)")
+print(f"Candidato 3: {candidato3} voto(s)")
