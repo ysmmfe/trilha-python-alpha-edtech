f2091b18a4dec17d8170388a37e81a9ef1a43e84
yasmim <yasmim.ferreira.200583@gmail.com>
2023-12-16 22:26:46 -0300

    09-write-file-txt

---
+++ C1 M1 - Python Essentials/C1 - A03 - Desvio Condicional/exercicio09.py
@@ -0,0 +1,14 @@
+# Trilha Python - Alpha EdTech
+# Autor: https://github.com/ysmmfe
+
+# Ciclo 1 > Módulo 1 - Python Essentials > Aula 03 - Desvio Condicional > Questão 9:
+# Como você escreve um arquivo de texto em Python e quais são as melhores práticas para abrir e fechar o arquivo após a leitura?
+# Crie um exemplo simples.
+
+# A melhor prática para fechar um arquivo após a abertura é utilizar o método "with open", que fecha o arquivo automaticamente.
+# Para criar um arquivo em Python, é preciso utilizar a estrutura with open("a", "b") as variável, em que "a" é o nome do arquivo
+# em formato .txt e "b" é o que você quer fazer com o arquivo, seja ler ("r"), escrever ("w"), adicionar algo ("a"), etc. A variável
+# é utilizada para armazenar o conteúdo que está dentro do arquivo, conforme o exemplo.
+
+with open("exemplo09.txt", "w") as arquivo:
+    arquivo.write("escrita da questão 9")
