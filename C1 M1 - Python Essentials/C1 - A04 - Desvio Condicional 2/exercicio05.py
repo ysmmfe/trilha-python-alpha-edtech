2a3921020b9bb5a9c703367259127a2ee9ec5232
yasmim <yasmim.ferreira.200583@gmail.com>
2023-12-16 22:38:19 -0300

    05-decimal-inteiro

---
+++ C1 M1 - Python Essentials/C1 - A04 - Desvio Condicional 2/exercicio05.py
@@ -0,0 +1,21 @@
+# Trilha Python - Alpha EdTech
+# Autor: https://github.com/ysmmfe
+
+# Ciclo 1 > Módulo 1 - Python Essentials > Aula 04 - Desvio Condicional 2 > Questão 5:
+# Faça um programa que peça um número e informe se o número é inteiro ou decimal. Dica: utilize uma função de arredondamento.
+
+# Utilizando apenas divisão inteira
+numero = float(input("Digite um número: "))
+
+if numero//1 == numero:
+    print(f"{int(numero)} é um número inteiro!")
+else:
+    print(f"{numero} é um número decimal!")
+
+# Utilizando a função de arredondamento
+num = float(input("Digite um número: "))
+
+if num == round(num):
+    print("O número é inteiro")
+else:
+    print("O número é decimal")
