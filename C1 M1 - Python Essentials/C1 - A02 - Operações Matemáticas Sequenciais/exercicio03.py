d112f76f4241c29318e0f1c01f0ac6e6a7a704af
yasmim <yasmim.ferreira.200583@gmail.com>
2023-12-16 21:47:54 -0300

    03-conversao-temperatura

---
+++ C1 M1 - Python Essentials/C1 - A02 - Operações Matemáticas Sequenciais/exercicio03.py
@@ -0,0 +1,14 @@
+# Trilha Python - Alpha EdTech
+# Autor: https://github.com/ysmmfe
+
+# Ciclo 1 > Módulo 1 - Python Essentials > Aula 02 - Operações Matemáticas Sequenciais > Questão 2:
+# A conversão de graus Fahrenheit para Celsius é dada pela expressão: C . 1.8 = F - 32
+# e a conversão de Kelvin para graus Celsius é dada por: C = k - 273.15. Faça um programa que recebe como entrada
+# a temperatura em graus Celsius e realize duas conversões: uma para Fahrenheit e outra para Kelvin.
+
+celsius = int(input("Informe a temperatura em °C: "))
+
+fahrenheit = celsius * 1.8 + 32
+kelvin = celsius + 273.15
+
+print(f"{celsius}°C são {fahrenheit}°F e {kelvin}°K")
