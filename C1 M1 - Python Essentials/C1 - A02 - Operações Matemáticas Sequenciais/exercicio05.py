4f9b7188f05f665211294387eb2580dcee3c4fe3
yasmim <yasmim.ferreira.200583@gmail.com>
2023-12-16 21:51:16 -0300

    05-salario-comissao

---
+++ C1 M1 - Python Essentials/C1 - A02 - Operações Matemáticas Sequenciais/exercicio05.py
@@ -0,0 +1,16 @@
+# Trilha Python - Alpha EdTech
+# Autor: https://github.com/ysmmfe
+
+# Ciclo 1 > Módulo 1 - Python Essentials > Aula 02 - Operações Matemáticas Sequenciais > Questão 5:
+# Faça um programa que leia o nome de um vendedor, o seu salário fixo e o total de vendas efetuadas por ele no mês (em dinheiro). 
+# Sabendo que este vendedor ganha 5% de comissão sobre suas vendas efetuadas, informar o total a receber no final do mês,
+# com duas casas decimais.
+
+nome = input("Informe o seu nome: ")
+salariofixo = float(input("Informe o seu salário: "))
+totalvendas = int(input("Informe o total em dinheiro das vendas efetuadas no mês: "))
+
+comissao = (5/100) * totalvendas
+salariototal = salariofixo + comissao
+
+print(f"\n{nome} vai receber R${salariototal:.2f} no fim do mês")
